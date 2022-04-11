bdaefd62a0bb3553e8c92275499002e92c2c5301
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-04-11 11:16:45 +0200

    Initial commit

---
+++ src/GrafikDel1.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GrafikDel1 extends Canvas {
+    private final int height = 600;
+    private final int width = 800;
+
+    public GrafikDel1() {
+        JFrame frame = new JFrame("Del 1");
+        this.setSize(width, height);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        GrafikDel1 exempel = new GrafikDel1();
+    }
+
+    public void paint(Graphics g){
+        drawHouse(g, 300, 200);
+        drawTreeArea(g, 10, 250, 440);
+    }
+
+    private void drawTreeArea(Graphics g, int x, int x2, int y){
+        drawTree(g, x, y);
+    }
+
+    private void drawTree(Graphics g, int x, int y){
+        g.setColor(new Color(0x460D0D));
+        g.fillRect(x+12, y+35, 6, 25);
+        g.setColor(new Color(0x185725));
+        g.fillOval(x, y, 30, 40);
+    }
+
+    private void drawHouse(Graphics g, int x, int y){
+        g.setColor(Color.black);
+        g.fillRect(x, y, 150, 300);
+        drawWindows(g, x, y);
+    }
+
+    private void drawWindows(Graphics g, int x, int y){
+        double rand;
+        for(int i = 0; i < 6; i++){
+            for(int a = 0; a < 3; a++){
+                if(i == 5 && a == 1){
+                    g.setColor(Color.white);
+                    g.drawRect(x+10+(a*50), y+10+(i*50), 30, 40);
+                }
+                else{
+                    rand = Math.random()*10;
+                    if(rand <= 5){
+                        g.setColor(Color.yellow);
+                        g.fillRect(x+15+(a*50), y+15+(i*50), 20, 20);
+                    }
+                    else{
+                        g.setColor(Color.white);
+                        g.drawRect(x+15+(a*50), y+15+(i*50), 20, 20);
+                    }
+                }
+            }
+        }
+    }
+
+}
